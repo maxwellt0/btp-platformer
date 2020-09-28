22686cf10fda6fdce5676595ada83ecbb6970980
Maxwellt <maksimvinnik@gmail.com>
2020-09-28 12:00:24 +0300

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private bool _isFacingRight = false;
+    private bool _isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float jumpForce;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
+
+        if (input > 0 && !_isFacingRight)
+        {
+            Flip();
+        }
+        else if (input < 0 && _isFacingRight) 
+        {
+            Flip();
+        }
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        _isFacingRight = !_isFacingRight;
+    }
+}
